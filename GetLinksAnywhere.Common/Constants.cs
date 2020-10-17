02755dd0f2c0a27053c4edc5d486995dd0dd9fb5
mrfishchev <dopingoo@gmail.com>
2020-10-18 00:25:02 +0300

    Added Extensions and Constants

---
+++ GetLinksAnywhere.Common/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace GetLinksAnywhere.Common
+{
+    public static class Constants
+    {
+        public const int MaxLengthOfChunk = 50;
+    }
+}
---
+++ GetLinksAnywhere.Common/Extensions/StringExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using GetLinksAnywhere.Model;
+
+namespace GetLinksAnywhere.Common.Extensions
+{
+    public static class StringExtension
+    {
+        public static Task<IEnumerable<Chunk>> SplitToChunks(this string data, 
+            int maxLengthOfChunk = Constants.MaxLengthOfChunk)
+        {
+            if (string.IsNullOrWhiteSpace(data))
+                return Task.FromResult(Enumerable.Empty<Chunk>());
+
+            return Task.Run(() =>
+            {
+                var pattern = @"(?:(.{1," + maxLengthOfChunk + @"})(?:\s|$)|(.+?)(?:\s|$))";
+
+                return Regex.Split(data, pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase)
+                    .Where(x => x.Length > 0)
+                    .Select(x => new Chunk(x));
+            });
+        }
+    }
+}
