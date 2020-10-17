624505125e6e839edd52c0553092ac61333b16e9
mrfishchev <dopingoo@gmail.com>
2020-10-18 00:25:32 +0300

    Added Chunk model

---
+++ GetLinksAnywhere.Model/Chunk.cs
@@ -0,0 +1,14 @@
+﻿namespace GetLinksAnywhere.Model
+{
+    public class Chunk
+    {
+        public Chunk(string content)
+        {
+            Content = content;
+        }
+
+        public string Content { get; set; }
+
+        public bool IsProcessed { get; set; }
+    }
+}
