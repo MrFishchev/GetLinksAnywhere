a467c58808297bfb754baa8a75d692f46c3dd3c7
mrfishchev <dopingoo@gmail.com>
2020-10-19 21:08:33 +0300

    Added missing tests

--- GetLinksAnywhere.Tests/Controllers/ProcessingControllerTests.cs
+++ GetLinksAnywhere.Tests/Controllers/ProcessingControllerTests.cs
@@ -1,6 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using GetLinksAnywhere.Controllers;
+using GetLinksAnywhere.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
 using NUnit.Framework;
 
 namespace GetLinksAnywhere.Tests.Controllers
@@ -8,6 +13,62 @@ namespace GetLinksAnywhere.Tests.Controllers
     [TestFixture]
     public class ProcessingControllerTests
     {
+        #region Fields
         
+        private ProcessingController _controller;
+        private Mock<IFinderService> _finderService;
+        private CancellationTokenSource _cancellationToken;
+
+        #endregion
+
+        #region SetUp
+
+        [SetUp]
+        public void SetUp()
+        {
+            _cancellationToken = new CancellationTokenSource();
+            _finderService = new Mock<IFinderService>();
+            _controller = new ProcessingController(_finderService.Object,
+                new NullLogger<ProcessingController>());
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public async Task FindLinksInText_DataIsNullOrWhiteSpace_ReturnStatusNoContent(string data)
+        {
+            var result = await _controller.FindLinksInText(data, _cancellationToken.Token);
+
+            Assert.That(result, Is.TypeOf<NoContentResult>());
+        }
+
+        [Test]
+        public async Task FindLinksInText_WhenCalled_CallFinderService()
+        {
+            var result = await _controller.FindLinksInText("data", _cancellationToken.Token);
+
+            _finderService.Verify(x => x.FindAllLinks(It.IsAny<string>(),
+                It.IsAny<int>(), It.IsAny<CancellationToken>()));
+        }
+
+        [Test]
+        public async Task FindLinksInText_WhenSuccess_ReturnOkWithResult()
+        {
+            _finderService.Setup(x => x.FindAllLinks(It.IsAny<string>(),
+                    It.IsAny<int>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => new List<string> {"result"});
+
+            var result = await _controller.FindLinksInText("data", _cancellationToken.Token);
+
+            Assert.That(result, Is.TypeOf<OkObjectResult>());
+            Assert.That(((OkObjectResult) result).Value,
+                Is.EquivalentTo(new List<string> {"result"}));
+        }
+
+        #endregion
     }
 }
